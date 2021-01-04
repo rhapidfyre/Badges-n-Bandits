18960f552e6c2e38a9113275c04fc69b92040619
Michael Harris <maharris84@hotmail.com>
2021-01-04 14:09:39 -0800

    GHMattiMySQL Dependency
    Added GHMMySQL Dependencies

--- src/bnbandits/fxmanifest.lua
+++ src/bnbandits/fxmanifest.lua
@@ -8,6 +8,7 @@ fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
+dependency 'ghmattimysql'
 resource_type 'gametype' { name = 'Badges & Bandits'}
 
 client_scripts {"ent_enum.lua","client/lib/*.lua","client/lang/*.lua","client/*.lua"}
--- src/bnbandits/server/lib/init.lua
+++ src/bnbandits/server/lib/init.lua
@@ -9,5 +9,45 @@ BB.scores   = {}
 BB.admins   = {}
 BB.reduce   = {points = 1,timer  = 60}
 
+BB.SQL = {
+    -- Execute and Forget
+    -- Executes 'cb' (callback) function on result with result as argument
+    -- Script doesn't wait for a response/result
+    EXECUTE = function(query,tbl,cb)
+      if not query then return error("No querystring given to CNR.SQL.") end
+      if not tbl then tbl = {} end
+      exports['ghmattimysql']:execute(query, tbl,
+        function(result) if cb then cb(result) end end
+      )
+    end,
+    
+    -- As EXECUTE
+    -- Script waits for a response
+    QUERY = function(query,tbl)
+      if not query then return error("No querystring given to CNR.SQL.") end
+      if not tbl then tbl = {} end
+      return ( exports['ghmattimysql']:executeSync(query, tbl) )
+    end,
+    
+    -- Fetches first column of the first row
+    -- Executes 'cb' (callback) function on result with result as argument
+    -- Runs without waiting on a return
+    SCALAR = function(query,tbl,cb)
+      if not query then return error("No querystring given to CNR.SQL.") end
+      if not tbl then tbl = {} end
+      exports['ghmattimysql']:scalar(query, tbl,
+        function(result) if cb then cb(result) end end
+      )
+    end,
+    
+    -- As SCALAR
+    -- Script waits for a response
+    RSYNC = function(query,tbl)
+      if not query then return error("No querystring given to CNR.SQL.") end
+      if not tbl then tbl = {} end
+      return ( exports['ghmattimysql']:scalarSync(query, tbl) )
+    end
+}
+
 -- Used by main script loop(s) to ensure metatable is ready
 BB.ready  = true
